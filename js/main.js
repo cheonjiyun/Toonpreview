2a35e14fe9e839d0e1e064cb221a93ed5c5d9249
cheonjiyun <gunbam715@gmail.com>
2024-03-05 22:03:23 +0900

    feat: 파일 업로드 클릭 - 썸네일 - 하나일때

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-import "./phone-type.js";
+// 폰 종류
+let phonetypes = document.querySelectorAll("input[name='phonetype']");
+let phone = document.querySelector(".phone");
+
+function changePhone() {
+    const phonetype = document.querySelector("input[name='phonetype']:checked");
+    phone.className = `phone ${phonetype.id}`;
+}
+
+for (const phonetype of phonetypes) {
+    phonetype.addEventListener("click", changePhone);
+}
+
+// 파일업로드
+let imageFile = document.querySelector("#btn-upload");
+imageFile.addEventListener("change", (event) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.addEventListener("load", () => {
+        document.querySelectorAll(
+            ".image-files"
+        )[0].style.background = `url(${reader.result})`;
+
+        document.querySelectorAll(
+            ".image-title"
+        )[0].innerText = `${event.target.files[0].name}`;
+    });
+});
